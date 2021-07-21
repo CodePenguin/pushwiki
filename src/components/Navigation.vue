54d952789cf2d0466ca49c526a4314a4627b0c66
David Lambert <codepenguin@gmail.com>
2021-07-20 21:47:47 -0400

    Changed navigation brand to use router-link component instead.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar navbar-expand-lg" v-bind:class="navbarClasses">
     <div class="container-fluid">
-      <a class="navbar-brand" href="./">{{title}}</a>
+      <router-link to="/" class="navbar-brand">{{title}}</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
