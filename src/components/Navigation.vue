30749c606acc12429380302eb5988e4086fa770f
David Lambert <codepenguin@gmail.com>
2021-07-20 21:35:26 -0400

    Changed brand link to the relative base.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar navbar-expand-lg" v-bind:class="navbarClasses">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">{{title}}</a>
+      <a class="navbar-brand" href="./">{{title}}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
