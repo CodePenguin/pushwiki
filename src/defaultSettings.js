789f1d772351be232c45eab6ab65de54637d8c01
David Lambert <codepenguin@gmail.com>
2021-07-23 21:59:58 -0400

    Added base typography and fixed colors on header.

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -15,11 +15,11 @@ export default {
       button: "md:hidden flex items-center px-3 py-2 border rounded bg-blue-900 text-white p-1 hover:text-black hover:bg-gray-200",
       brand: "block h-6 md:h-",
       header: "flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-blue-900 text-white py-4 px-4 relative",
-      nav: "absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0",
-      navLink: "block py-1 text-gray-600 hover:underline"
+      nav: "absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-gray-100 shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0",
+      navLink: "block py-1 hover:underline text-gray-600 md:text-white"
     },
     page: {
-      article: "col p-3",
+      article: "col p-3 prose",
       markdown: {
         pre: "border p-1",
         table: "table"
