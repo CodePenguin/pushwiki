7e503bfee2e5e6a363e786667992ba6ace88a0da
David Lambert <codepenguin@gmail.com>
2021-08-04 22:15:47 -0400

    Reintegrated the TailwindCSS changes and added specific safelist values to keep the core small but still allow lots of customization options.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import router from './router'
 
 if (process.env.VUE_APP_BUNDLE_TYPE !== 'slim') {
   require('highlightCSS')
-  require('@tailwindcss/typography/dist/typography.min.css')
-  require('tailwindcss/dist/tailwind.min.css')
+  require('./tailwind.css')
 }
 
 createApp(App).use(router).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  darkMode: false,
+  plugins: [require('@tailwindcss/typography')],
+  purge: {
+    content: ['./src/**/*.{vue,ts}'],
+    safelist: {
+      greedy: [
+        /^(sm:|md:|lg:|xl:)?(?:bg-|text-)/,
+        /^(sm:|md:|lg:|xl:)?(?:border-|rounded-|shadow-)/,
+        /^(sm:|md:|lg:|xl:)?(?:container)$/,
+        /^(sm:|md:|lg:|xl:)?(?:grid-|col-|row-|table-)/,
+        /^(sm:|md:|lg:|xl:)?(?:m|p)[blrtxy]?-/,
+        /^(sm:|md:|lg:|xl:)?(?:not-sr-only|sr-only)$/,
+        /^(sm:|md:|lg:|xl:)?(?:prose|prose-)/,
+        /^(sm:|md:|lg:|xl:)?(?:visible|invisible|hidden)$/,
+        /^(sm:|md:|lg:|xl:)?z-$/
+      ]
+    }
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  }
+}
