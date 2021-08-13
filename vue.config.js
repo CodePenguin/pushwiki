dfd24ef598dfa300894e439d6f5fb87628b79010
David Lambert <codepenguin@gmail.com>
2021-08-13 06:37:12 -0400

    Removed the injection of the JS file into the HTML as it doesn't add much value and reduces the ease of manually modifying the HTML file in production if required.

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const path = require('path')
-const fs = require('fs')
-const UglifyJS = require('uglify-js')
-
 module.exports = {
-  indexPath: 'pushwiki.html',
   publicPath: '',
   css: {
     extract: false
@@ -30,24 +23,6 @@ module.exports = {
       filename: 'pushwiki.js',
       chunkFilename: 'pushwiki.js'
     },
-    plugins: [
-      {
-        apply: (compiler) => {
-          if (process.env.NODE_ENV !== 'production') return
-          compiler.hooks.done.tap('InlinePlugin', () => {
-            //Combine HTML and JS into a single deployment file
-            fs.copyFileSync(path.resolve(__dirname, './dist/pushwiki.html'), path.resolve(__dirname, './dist/index.html'))
-            const scriptContents = fs.readFileSync(path.resolve(__dirname, './dist/pushwiki.js'), 'utf8')
-            const outputScriptContents = UglifyJS.minify(scriptContents).code.replace(/\$/g, '$$$$')
-            const htmlContents = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf8')
-            const contents = htmlContents
-              .replace('<link href="pushwiki.js" rel="preload" as="script">', '')
-              .replace(' src="pushwiki.js"></script>', '>' + outputScriptContents + '</script>')
-            fs.writeFileSync(path.resolve(__dirname, './dist/index.html'), contents)
-          })
-        }
-      }
-    ],
     resolve: {
       alias: {
         'highlight.js/lib/common': process.env.VUE_APP_BUNDLE_TYPE === 'core' ? 'highlight.js/lib/core' : 'highlight.js/lib/common'
